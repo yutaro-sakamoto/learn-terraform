baa8f217c071f70b85384ecd93db736627a9fe4b
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-12-09 20:02:21 +0900

    format: format 01-ec2/main.tf

--- 01-ec2/main.tf
+++ 01-ec2/main.tf
@@ -8,18 +8,18 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 }
 
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id            = aws_vpc.main.id
   availability_zone = "ap-northeast-1a"
-  cidr_block = "10.0.0.0/24"
+  cidr_block        = "10.0.0.0/24"
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-023ff3d4ab11b2525"
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.main.id
+  subnet_id     = aws_subnet.main.id
 }
