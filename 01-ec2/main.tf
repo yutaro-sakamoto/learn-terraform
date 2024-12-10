516d0383fd2c19df3d13c6bc053b59b27ef4e879
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-12-10 19:22:10 +0900

    feat: enhance an ec2 instance

--- 01-ec2/main.tf
+++ 01-ec2/main.tf
@@ -22,4 +22,26 @@ resource "aws_instance" "example" {
   ami           = "ami-023ff3d4ab11b2525"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main.id
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data_replace_on_change = true
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World!" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
+}
+
+resource "aws_security_group" "instance" {
+  name   = "terraform-example-instance"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
