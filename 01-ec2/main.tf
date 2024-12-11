6673cb9bed8fbb16c607ca43a1af6ed64b6ad234
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-12-11 21:34:20 +0900

    feat: create an ALB

--- 01-ec2/main.tf
+++ 01-ec2/main.tf
@@ -17,26 +17,127 @@ resource "aws_vpc" "main" {
   instance_tenancy = "default"
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   availability_zone = "ap-northeast-1a"
   cidr_block        = "10.0.0.0/24"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-023ff3d4ab11b2525"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
 
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
 
-  user_data_replace_on_change = true
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "sub2" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.2.0/24"
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-023ff3d4ab11b2525"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
     #!/bin/bash
     echo "Hello, World!" > index.html
     nohup busybox httpd -f -p ${var.server_port} &
     EOF
+}
+
+locals {
+  subnet_ids = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = local.subnet_ids
+  target_group_arns    = [aws_lb_target_group.example.arn]
+  health_check_type    = "ELB"
+  min_size             = 2
+  max_size             = 2
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name   = "terraform-example-alb"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.main.id, aws_subnet.sub1.id]
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_target_group" "example" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    port                = var.server_port
+    protocol            = "HTTP"
+    timeout             = 3
+    interval            = 15
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = "404"
+    }
+  }
 }
 
 resource "aws_security_group" "instance" {
@@ -51,8 +152,23 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_lb_listener_rule" "example" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP of the instance"
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
