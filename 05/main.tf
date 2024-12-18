b2c38c4cb57ea69010b2747c168fb9d7b80671cd
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-12-18 22:04:29 +0900

    add: a list variable example

--- 05/main.tf
+++ 05/main.tf
@@ -6,3 +6,13 @@ resource "aws_iam_user" "example" {
   count = 3
   name  = "user-${count.index}"
 }
+
+variable "user_names" {
+  type    = list(string)
+  default = ["neo", "trinity", "morpeus"]
+}
+
+resource "aws_iam_user" "example2" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
